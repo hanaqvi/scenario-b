cc10f89213419958578ff3f4ec433135453b65b6
Hasan Naqvi <hasansyednaqvi1@gmail.com>
2026-05-28 16:57:11 +0100

    started framework

--- framework.ino/framework.ino.ino
+++ framework.ino/framework.ino.ino
@@ -6,4 +6,72 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  scanLeft();
+  
+  scanRight();
+
 }
+
+void moveForward(int motorPinL, int motorPinR, int motorPinB, int motorPinF) {
+
+  digitalWrite(motorPinL, LOW);
+  digitalWrite(motorPinR, HIGH);
+  digitalWrite(motorPinB, LOW);
+  digitalWrite(motorPinF, LOW);
+
+}
+
+void moveForward() {
+
+  digitalWrite(motorPinL, LOW);
+  digitalWrite(motorPinR, HIGH);
+  digitalWrite(motorPinB, LOW);
+  digitalWrite(motorPinF, LOW);
+
+}
+
+void moveBackward() {
+
+  digitalWrite(motorPinL, HIGH);
+  digitalWrite(motorPinR, LOW);
+  digitalWrite(motorPinB, LOW);
+  digitalWrite(motorPinF, LOW);
+
+}
+
+void moveLeft() {
+
+  digitalWrite(motorPinL, LOW);
+  digitalWrite(motorPinR, LOW);
+  digitalWrite(motorPinB, HIGH);
+  digitalWrite(motorPinF, LOW);
+
+}
+
+void moveRight() {
+
+  digitalWrite(motorPinL, LOW);
+  digitalWrite(motorPinR, LOW);
+  digitalWrite(motorPinB, LOW);
+  digitalWrite(motorPinF, HIGH);
+
+}
+
+
+void scanLeft() {
+
+  // move left and scan for gaps
+
+}
+
+void scanRight() {
+
+  // move right and scan for gaps
+
+}
+
+
+
+
+
+
